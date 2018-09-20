db8942b88277189d48b267fde5bd33c6b9f911a4
aaronlee98 <43352241+aaronlee98@users.noreply.github.com>
2018-09-19 18:13:58 -0700

    Added Switch_Init which initializes Port F and Port D, Initialize Switches before initializing PWM's

--- Project1/PWM.c
+++ Project1/PWM.c
@@ -129,3 +129,35 @@ void GPIOPortF_Handler(void){ // called on touch of either SW1 or SW2
 
 	//Edit for future usage: Turn direction into a two bit GPIO_PORTA_DATA_R for bits 3 and 2 so it can easily connect to LED
 }
+
+void Switch_Init(void){  
+	unsigned long volatile delay;
+  SYSCTL_RCGC2_R |= 0x00000020; // (a) activate clock for port F
+  delay = SYSCTL_RCGC2_R;
+  GPIO_PORTF_LOCK_R = 0x4C4F434B; // unlock GPIO Port F
+  GPIO_PORTF_CR_R = 0x1F;         // allow changes to PF4,0
+  GPIO_PORTF_DIR_R &= ~0x11;    // (c) make PF4,0 in (built-in button)
+	GPIO_PORTF_DIR_R |=  0x0E;
+  GPIO_PORTF_AFSEL_R &= ~0x1F;  //     disable alt funct on PF4,0
+  GPIO_PORTF_DEN_R |= 0x1F;     //     enable digital I/O on PF4,0
+  GPIO_PORTF_PCTL_R &= ~0x000FFFFF; //  configure PF4,0 as GPIO
+  GPIO_PORTF_AMSEL_R &= ~0x1F;  //     disable analog functionality on PF4,0
+  GPIO_PORTF_PUR_R |= 0x11;     //     enable weak pull-up on PF4,0
+  GPIO_PORTF_IS_R &= ~0x11;     // (d) PF4,PF0 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x11;    //     PF4,PF0 is not both edges
+  GPIO_PORTF_IEV_R &= ~0x11;    //     PF4,PF0 falling edge event
+  GPIO_PORTF_ICR_R = 0x11;      // (e) clear flags 4,0
+  GPIO_PORTF_IM_R |= 0x11;      // (f) arm interrupt on PF4,PF0
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00400000; // (g) priority 2
+  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
+	
+	SYSCTL_RCGC2_R 			|= 0x00000002;     	// 1) B clock
+  delay 							 = SYSCTL_RCGC2_R;  // delay   
+  GPIO_PORTD_CR_R 		|= 0x3F;          	// allow changes to PB5-0       
+  GPIO_PORTD_AMSEL_R 	 = 0x00;        		// 3) disable analog function
+  GPIO_PORTD_PCTL_R 	 = 0x00000000;   		// 4) GPIO clear bit PCTL  
+  GPIO_PORTD_DIR_R 		|= 0x3F;         		// 5) Output PB0-5
+  GPIO_PORTD_AFSEL_R 	 = 0x00;        		// 6) no alternate function
+  GPIO_PORTD_DEN_R 		|= 0x3F;         		// 7) enable digital pins PB2-PB0
+	delay = delay;													// 8) Dummy Command
+}
